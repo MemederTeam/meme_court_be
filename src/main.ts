aa6c10993c6d4ee775d3df47c5cde332aca3eba3
yeseul0 <choiyeseul@g.hongik.ac.kr>
2025-12-01 21:23:53 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  // Validation Pipe 글로벌 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없는 속성 제거
+      forbidNonWhitelisted: false, // 추가 속성 있어도 에러 안남
+      transform: true, // 타입 자동 변환
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
